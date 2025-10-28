098dd6f57c758c42e64846154fc49d95518497ee
EXsiDe <petruhin.paulus@yandex.ru>
2025-10-28 23:08:22 +0300

    Remove the retry logic

--- api/v1/tasks/ocr_tasks.py
+++ api/v1/tasks/ocr_tasks.py
@@ -22,46 +22,43 @@ MESSAGE_KEY = "message"
 TEXT_KEY = "text"
 
 
-@celery_app.task(bind=True, retry_backoff=True, max_retries=5, acks_late=True)
+@celery_app.task(bind=True)
 def process_image(self: Task, filename: str, image_data: bytes):
-    try:
-        image_hash = hashlib.md5(image_data).hexdigest()
-        self.update_state(
-            state=states.STARTED,
-            meta={
-                IMAGE_HASH_KEY: image_hash,
-                FILENAME_KEY: filename,
-                MESSAGE_KEY: "Processing image...",
-            },
-        )
+    image_hash = hashlib.md5(image_data).hexdigest()
+    self.update_state(
+        state=states.STARTED,
+        meta={
+            IMAGE_HASH_KEY: image_hash,
+            FILENAME_KEY: filename,
+            MESSAGE_KEY: "Processing image...",
+        },
+    )
 
-        cached_result = redis_client.get(image_hash)
-        if cached_result is not None:
-            self.update_state(
-                state=states.SUCCESS,
-                meta={
-                    IMAGE_HASH_KEY: image_hash,
-                    FILENAME_KEY: filename,
-                    MESSAGE_KEY: "Result from cache",
-                    TEXT_KEY: cached_result,
-                },
-            )
-            return cached_result
-
-        image = Image.open(io.BytesIO(image_data))
-        text = pytesseract.image_to_string(image, lang="rus+eng")
-
-        redis_client.set(image_hash, text)
-        redis_client.expire(image_hash, 3600)
+    cached_result = redis_client.get(image_hash)
+    if cached_result is not None:
         self.update_state(
             state=states.SUCCESS,
             meta={
                 IMAGE_HASH_KEY: image_hash,
                 FILENAME_KEY: filename,
-                MESSAGE_KEY: "Image processed and cached",
-                TEXT_KEY: text,
+                MESSAGE_KEY: "Result from cache",
+                TEXT_KEY: cached_result,
             },
         )
-        return text
-    except Exception as e:
-        self.retry(exc=e)
+        return cached_result
+
+    image = Image.open(io.BytesIO(image_data))
+    text = pytesseract.image_to_string(image, lang="rus+eng")
+
+    redis_client.set(image_hash, text)
+    redis_client.expire(image_hash, 3600)
+    self.update_state(
+        state=states.SUCCESS,
+        meta={
+            IMAGE_HASH_KEY: image_hash,
+            FILENAME_KEY: filename,
+            MESSAGE_KEY: "Image processed and cached",
+            TEXT_KEY: text,
+        },
+    )
+    return text
